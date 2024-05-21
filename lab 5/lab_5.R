dc7211fc92c0da79338f5693981f6d9fd360f374
P <petrubraha@gmail.com>
2024-05-21 13:29:51 +0300

    lab 5 done

---
+++ lab 5/lab_5.R
@@ -0,0 +1,16 @@
+ex1 <- function(x, p)
+{
+  U <- runif(1)
+  actual <- 0
+  
+  for(i in seq_along(pi))
+  {
+    actual <- actual + pi[i]
+    if(U <= actual)
+      return(x[i])
+  }
+  
+  return(x[length(x)])
+}
+
+print(ex1(c(1, 2, 3), c(0.2, 0.5, 0.3)))
