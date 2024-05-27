e36f60b7313c666a55dd11f6f699a4d769229b8e
P <petrubraha@gmail.com>
2024-05-27 20:51:40 +0300

    correct lab 11 solution

--- lab 11/lab_5.R
+++ lab 11/lab_5.R
@@ -1,19 +1,22 @@
 generation <- function(x, p)
 {
-  U <- runif(1)
-  actual <- 0
+  if(sum(p) != 1)
+    return(-1) # error 
   
-  for(i in seq_along(p))
-  {
-    actual <- actual + p[i]
-    if(U <= actual)
+  u <- runif(1)
+  sum <- cumsum(p)
+  for(i in 1:length(x))
+    if(u <= sum[i])
       return(x[i])
-  }
-  
+
   return(x[length(x)])
 }
 
 ex1 <- function()
 {
-  cat("output:", generation(c(1, 2, 3), c(0.2, 0.5, 0.3)), "\n")
+  temp = vector()
+  for(i in 1:1000)
+    temp[i] <- generation(c(1, 2, 3), c(0.2, 0.5, 0.3))
+  distribution <- table(temp)
+  cat("output:", distribution, "\n")
 }
